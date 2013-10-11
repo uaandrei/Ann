a30f1c8417b5e728d04d6276319d706e2c17c164
uaandrei <ua.andrei@gmail.com>
2013-10-11 19:17:04 +0300

    explicit select

--- anunturi/models/advert_model.php
+++ anunturi/models/advert_model.php
@@ -14,7 +14,7 @@ class Advert_Model extends MY_MODEL
 
     function getByCategoryId($categoryId)
     {
-        $sql = "select * from adverts
+        $sql = "select adverts.id, category_id, user_id, district, city, type, title, description, price, currency, date, filename from adverts
                 join (select file_id, advert_id from advert_files
                     join files on files.id = advert_files.file_id
                     where filename like '%thumb%'
@@ -42,7 +42,7 @@ class Advert_Model extends MY_MODEL
         // $this->db->join(ADVERT_FILES_TABLE, $this->tableName . '.id=' . ADVERT_FILES_TABLE . '.advert_id');
         // $this->db->join(FILES_TABLE, FILES_TABLE . '.id=' . ADVERT_FILES_TABLE . '.file_id');
         // $this->db->like(FILES_TABLE . '.filename', 'thumb');
-        $sql = "select * from adverts
+        $sql = "select adverts.id, category_id, user_id, district, city, type, title, description, price, currency, date, filename from adverts
                 join (select file_id, advert_id from advert_files
                     join files on files.id = advert_files.file_id
                     where filename like '%thumb%'
