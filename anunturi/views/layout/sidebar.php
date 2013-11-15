b7c08729d1d5a8bed2ed62ba0edfe7a2dc41f6d7
uaandrei <ua.andrei@gmail.com>
2013-11-15 20:09:19 +0200

    added space between category name and icon

--- anunturi/views/layout/sidebar.php
+++ anunturi/views/layout/sidebar.php
@@ -3,19 +3,19 @@
 		<ul class="nav">
 			<li class="nav-header">CATEGORII</li>
 			<?php
-			foreach ( $categories as $category ) {
-				echo '<li';
-				if ($active_page == $category->id) {
-					echo ' class="active"';
-				}
-				echo '><a href="' . base_url () . 'advert/category/' . $category->id . '">';
-				if (! is_null ( $category->icon )) {
-					echo '<i class="' . $category->icon . '"></i>';
-				}
-				echo $category->name;
-				echo '</a></li>';
-			}
-			?>
+                foreach ($categories as $category) {
+                    echo '<li';
+                    if ($active_page == $category->id) {
+                        echo ' class="active"';
+                    }
+                    echo '><a href="' . base_url() . 'advert/category/' . $category->id . '">';
+                    if (! is_null($category->icon)) {
+                        echo '<i class="' . $category->icon . '"></i>';
+                    }
+                    echo "&nbsp;" . $category->name;
+                    echo '</a></li>';
+                }
+            ?>
 		</ul>
 	</div>
 </div>
