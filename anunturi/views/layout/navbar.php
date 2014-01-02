8e5f80e42217f9477aa977707656a1ed75d009a5
andrei.ursta <ua.andrei@gmail.com>
2014-01-02 13:31:55 +0200

    updated nav header

--- anunturi/views/layout/navbar.php
+++ anunturi/views/layout/navbar.php
@@ -2,9 +2,13 @@
 	<div class="container">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-				<span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">Anunturi</a>
+			<a class="navbar-brand" href="#">
+				<strong style="font-family: cursive; font-size: larger; color: darkorange;">up.ro</strong>
+			</a>
 		</div>
 		<div class="collapse navbar-collapse">
 			<ul class="nav navbar-nav">
