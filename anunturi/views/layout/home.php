b3b5b75201a4158fad39e9ea438b6950421630e6
andrei.ursta <ua.andrei@gmail.com>
2014-02-16 15:00:35 +0200

    concept for home page

--- anunturi/views/layout/home.php
+++ anunturi/views/layout/home.php
@@ -1,1 +1,7 @@
-Hello, welcome!
+<div class="jumbotron">
+	<h1>Bine ai venit!</h1>
+	<p>up.ro</p>
+	<p>
+		<a class="btn btn-primary btn-lg" role="button" href="<?php echo base_url();?>user/newAdvert">Adauga Anunt</a>
+	</p>
+</div>
